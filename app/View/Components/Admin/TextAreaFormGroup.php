3873995ab833ed66441765a1cc227539ab6edd43
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-09-14 05:05:07 +0600

    TextArea component;

--- app/View/Components/Admin/TextAreaFormGroup.php
+++ app/View/Components/Admin/TextAreaFormGroup.php
@@ -9,17 +9,15 @@ use App\View\BaseComponent;
 class TextAreaFormGroup extends BaseComponent
 {
     public $name;
-    public $type;
     public $placeholder;
     public $label;
     public $errors;
     public $required;
     public $value;
 
-    public function __construct($name, $type, $placeholder, $label, $errors = [], $required = true, $value = null)
+    public function __construct($name, $placeholder, $label, $errors = [], $required = true, $value = null)
     {
         $this->name = $name;
-        $this->type = $type;
         $this->placeholder = $placeholder;
         $this->label = $label;
         $this->errors = $errors;
