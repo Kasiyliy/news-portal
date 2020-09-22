ea6363d1f28f8d5ae07b4f3daac8577af1571d05
ulanzheniserov <zheniserov@gmail.com>
2020-09-23 02:59:38 +0600

    index page update

--- resources/views/modules/front/pages/index.blade.php
+++ resources/views/modules/front/pages/index.blade.php
@@ -3,12 +3,12 @@
 @section('styles')
     <link rel="stylesheet" href="{{asset('modules/front/assets/css/swiper.min.css')}}">
     <style>
-        .swiper-container {
+        .swiper-container.slider {
             width: 100%;
             height: 500px;
         }
 
-        .swiper-slide {
+        .swiper-slide.slider {
             text-align: center;
             font-size: 18px;
             background: #fff;
@@ -27,7 +27,7 @@
             align-items: center;
         }
 
-        .swiper-pagination{
+        .swiper-pagination.slider {
             display: flex;
             flex-direction: column;
             justify-content: center;
@@ -38,13 +38,22 @@
             background-color: rgba(0, 101, 109, 0.5);;
         }
 
-        .swiper-text h1{
+        .swiper-text h1 {
             font-size: 22px;
             line-height: 25px;
         }
 
-        .swiper-img img{
+        .swiper-img img {
             width: 100%;
+        }
+
+        .swiper-container.calendar {
+            width: 100%;
+            height: auto;
+        }
+
+        .swiper-slide.calendar {
+            background: #FFFFFF;
         }
 
         .swiper-pagination-bullet {
@@ -56,17 +65,18 @@
         }
 
 
-
-
         .fa {
             font-size: 0.5em;
         }
+
         table {
             width: 100%;
         }
+
         th {
             text-align: center;
         }
+
         td {
             text-align: center;
             font-size: 14px;
@@ -77,26 +87,35 @@
 
 
         }
-        td.day{
+
+        td.day {
             color: rgba(0, 0, 0, 0.95);
             font-family: Roboto;
             font-style: normal;
             font-weight: 500;
             text-transform: lowercase;
         }
+
         td,
         #year {
             font-family: monospace;
         }
+
         .hover {
             background: #eee;
         }
+
+        td.day.active {
+            color: red;
+        }
+
         #about {
             font-size: 5em !important;
             position: absolute;
             top: -35px;
             right: 15px;
         }
+
         #about a {
             text-decoration: none;
         }
@@ -203,8 +222,8 @@
         </div>
     </section>
 
-    <section class="about" id ="about_section" >
-        <div class="container" >
+    <section class="about" id="about_section">
+        <div class="container">
             <div class="row">
                 <div class="about__image col-12 col-lg-6 col-md-6 ">
                     <img src="{{asset('modules/front/assets/img/about.png')}}" alt="about">
@@ -249,10 +268,10 @@
 
     <section class="slider">
         <div class="container">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
+            <div class="swiper-container slider">
+                <div class="swiper-wrapper slider">
                     @foreach($slider as $s)
-                        <div class="swiper-slide row slider-reverse-block">
+                        <div class="swiper-slide slider row slider-reverse-block">
                             <div class="swiper-text col-md-12 col-lg-5 ml-5">
                                 <h1>{{$s->title}}</h1>
                             </div>
@@ -263,31 +282,49 @@
                     @endforeach
                 </div>
                 <!-- Add Pagination -->
-                <div class="swiper-pagination" id="swiper-pagination"></div>
+                <div class="swiper-pagination slider" id="swiper-pagination"></div>
             </div>
         </div>
     </section>
 
-    <section class="calendar" id ="event">
+    <section class="calendar" id="event">
         <div class="container">
             <h1>Іс-шара күнтізбесі</h1>
             <div class="calendar__inner row pb-5 justify-content-around">
                 <div class="calendar__general col-md-12 col-lg-4">
-                        <h1 class="text-center"><a id="left" ><i class="fa fa-chevron-left"> </i></a><span>&nbsp;</span><span id="month"> </span><span>&nbsp;</span><span id="year"> </span><span>&nbsp;</span><a id="right"><i class="fa fa-chevron-right"> </i></a></h1>
-
-                            <table class="table"></table>
-
+                    <h1 class="text-center"><a id="left"><i class="fa fa-chevron-left"> </i></a><span>&nbsp;</span><span
+                            id="month"> </span><span>&nbsp;</span><span id="year"> </span><span>&nbsp;</span><a
+                            id="right"><i class="fa fa-chevron-right"> </i></a></h1>
+                    <table class="table"></table>
                 </div>
                 <div class="calendar__detail col-md-12 col-lg-6">
-                    <h2>30 тамыз </h2>
-                    <h3>Конституция күні</h3>
-                    <div class="calendar__detail-button row">
-                        <div class="col-12 col-md-6 calendar__button left mt-3">
-                            <button>Толық көру</button>
+                    <h2 id="calendar-title">30 тамыз </h2>
+                    <div class="swiper-container calendar">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide calendar">
+                                <h3>Конституция күні</h3>
+                                <div class="calendar__detail-button row">
+                                    <div class="col-12 col-md-6 calendar__button left mt-3">
+                                        <button>Толық көру</button>
+                                    </div>
+                                    <div class="col-12 col-md-6 calendar__button right mt-3">
+                                        <button onclick="location.href='{{route('event.send')}}';">Іс-шараны ұсыну</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide calendar">
+                                <h3>Конституция күні</h3>
+                                <div class="calendar__detail-button row">
+                                    <div class="col-12 col-md-6 calendar__button left mt-3">
+                                        <button>Толық көру</button>
+                                    </div>
+                                    <div class="col-12 col-md-6 calendar__button right mt-3">
+                                        <button onclick="location.href='{{route('event.send')}}';">Іс-шараны ұсыну</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-12 col-md-6 calendar__button right mt-3">
-                            <button onclick="location.href='{{route('event.send')}}';">Іс-шараны ұсыну</button>
-                        </div>
+                        <div class="swiper-pagination"></div>
                     </div>
                 </div>
             </div>
@@ -300,12 +337,12 @@
                 <h1>Бизнес-идея</h1>
                 <div class="row">
                     @foreach($business_categories as $business_category)
-                    <div class="business__item col-10 mx-auto col-lg-4  text-center">
-                        <a class="d-flex" href="{{route('business',$business_category->id)}}">
-                            <img src="{{asset($business_category->image_path)}}" alt="tractor" height="36">
-                            <p>{{$business_category->name}}</p>
-                        </a>
-                    </div>
+                        <div class="business__item col-10 mx-auto col-lg-4  text-center">
+                            <a class="d-flex" href="{{route('business',$business_category->id)}}">
+                                <img src="{{asset($business_category->image_path)}}" alt="tractor" height="36">
+                                <p>{{$business_category->name}}</p>
+                            </a>
+                        </div>
                     @endforeach
                 </div>
 
@@ -313,21 +350,21 @@
         </div>
     </section>
 
-    <section class="news" id ="news">
+    <section class="news" id="news">
         <div class="container">
             <div class="news__inner">
-                <h1 >Жаңалықтар мен хабарландырулар </h1>
+                <h1>Жаңалықтар мен хабарландырулар </h1>
                 <div class="row">
-                        @foreach($news as $n)
-                            <div class="col-12 col-lg-3">
-                                <a href="{{route('news.detail', $n->id)}}">
-                                    <img src="{{asset($n->image_path)}}" alt="" width="210" height="143.17">
-                                    <p >{{strlen($n->title) > 111 ?
+                    @foreach($news as $n)
+                        <div class="col-12 col-lg-3">
+                            <a href="{{route('news.detail', $n->id)}}">
+                                <img src="{{asset($n->image_path)}}" alt="" width="210" height="143.17">
+                                <p>{{strlen($n->title) > 111 ?
                                     mb_substr($n->title,0,111)."..."
                                             : $n->title}}</p>
-                                </a>
-                            </div>
-                        @endforeach
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
 
             </div>
@@ -350,7 +387,7 @@
             }
         }
 
-        var swiper = new Swiper('.swiper-container', {
+        var swiper = new Swiper('.swiper-container.slider', {
             direction: screenSize < 992 ? 'horizontal' : 'vertical',
             pagination: {
                 el: '.swiper-pagination',
@@ -359,6 +396,13 @@
             navigation: {
                 nextEl: '.swiper-button-next-custom',
                 prevEl: '.swiper-button-prev-custom',
+            },
+        });
+        var swiper = new Swiper('.swiper-container.calendar', {
+            pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+                clickable: true,
             },
         });
     </script>
@@ -375,6 +419,7 @@
                     }
                     return result;
                 }
+
                 Date.prototype.monthDays = function () {
                     var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
                     return d.getDate();
@@ -407,6 +452,7 @@
                         "Желтоқсан"
                     ]
                 };
+                let title = document.getElementById('calendar-title');
                 var start = new Date(d.getFullYear(), d.getMonth()).getDay();
                 var cal = [];
                 var day = 1;
@@ -420,7 +466,12 @@
                         } else {
                             if (i === 1 && j < start) {
                                 cal[i].push("<td>&nbsp;</td>");
-                            } else {
+                            }
+                            else if(day === new Date().getDate() && d.getMonth() === new Date().getMonth()){
+                                cal[i].push('<td class="day active">' + day++ + "</td>");
+                                title.innerHTML = `${new Date().getDate()} ${details.months[d.getMonth()]}`
+                            }
+                            else {
                                 cal[i].push('<td class="day">' + day++ + "</td>");
                             }
                         }
@@ -442,8 +493,18 @@
                     })
                     .mouseout(function () {
                         $(this).removeClass("hover");
-                    });
+                    })
+                    .click(function(){
+                        let days = document.getElementsByClassName("day");
+                        for(let i=0; i<days.length; i++){
+                            days[i].className = "day";
+                        }
+                        title.innerHTML = `${$(this)[0].outerText} ${details.months[d.getMonth()]}`
+                        $(this).addClass("active")
+                    })
+                ;
             }
+
             $("#left").click(function () {
                 $("table").text("");
                 if (currentDate.getMonth() === 0) {
