dc063939e471c17b2dd60d4bcd63e70bd974034d
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-10-02 18:16:47 +0600

    Error fix;

--- app/Http/Controllers/Web/V1/Front/GuideFaqController.php
+++ app/Http/Controllers/Web/V1/Front/GuideFaqController.php
@@ -15,11 +15,14 @@ class GuideFaqController extends WebBaseController
     {
         $categories = GuideCategory::with('contents')->get();
         $i = 0;
-        $currentCategory = $categories->first();
         if ($request->category_id) {
             $currentCategory = $categories->where('id', $request->category_id)->first();
-            if (!$currentCategory) throw new WebServiceExplainedException('Не найдено!');
         }
+        else {
+            $currentCategory = $categories->first();
+        }
+        if (!$currentCategory) throw new WebServiceExplainedException('Не найдено!');
+
         return $this->frontView('pages.guide-faqs.guide', compact('categories', 'i', 'currentCategory'));
     }
 
@@ -30,11 +33,15 @@ class GuideFaqController extends WebBaseController
     public function faqs(Request $request) {
         $categories = FaqCategory::with('questions')->get();
         $i = 0;
-        $currentCategory = $categories->first();
         if ($request->category_id) {
             $currentCategory = $categories->where('id', $request->category_id)->first();
             if (!$currentCategory) throw new WebServiceExplainedException('Не найдено!');
         }
+        else {
+            $currentCategory = $categories->first();
+        }
+        if (!$currentCategory) throw new WebServiceExplainedException('Не найдено!');
+
         return $this->frontView('pages.guide-faqs.faqs', compact('categories', 'i', 'currentCategory'));
 
     }
