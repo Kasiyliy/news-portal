12de5b7b5e449f78e2a28a767e89652ebbbe59f7
daur <daur-lion@mail.ru>
2020-10-03 22:03:25 +0600

    Forum fix v1

--- routes/web/V1/web.php
+++ routes/web/V1/web.php
@@ -87,11 +87,11 @@ Route::group(['namespace' => 'Front'], function () {
     Route::get('/forum/questionnaire/{id}', ['uses' => 'ForumController@questionnaire', 'as' => 'forum.questionnaire'])->where('id', '[0-9]+');
     Route::get('/forum/questionnaire-list', ['uses' => 'ForumController@questionnaireList', 'as' => 'forum.questionnaire.list']);
     Route::get('/forum/questionnaire/post', ['uses' => 'ForumController@questionnairePost', 'as' => 'forum.questionnaire.post']);
-    Route::get('/forum/categories', ['uses' => 'ForumController@categories', 'as' => 'forum.categories'])->middleware('auth');
+    Route::get('/forum/categories', ['uses' => 'ForumController@categories', 'as' => 'forum.categories']);
     Route::get('/forum/category-list/{id}', ['uses' => 'ForumController@categoryList', 'as' => 'forum.category.list'])->where('id', '[0-9]+');
-    Route::get('/forum/category-detail/{id}', ['uses' => 'ForumController@categoryDetail', 'as' => 'forum.category.detail'])->where('id', '[0-9]+')->middleware('auth');
+    Route::get('/forum/category-detail/{id}', ['uses' => 'ForumController@categoryDetail', 'as' => 'forum.category.detail'])->where('id', '[0-9]+');
     Route::post('/forum/category-detail/post/{id}', ['uses' => 'ForumController@categoryDetailPost', 'as' => 'forum.category.detail.post'])->where('id', '[0-9]+')->middleware('auth');
-    Route::get('/forum/messages/{id}', ['uses' => 'ForumController@categoryMessages', 'as' => 'forum.category.messages'])->where('id', '[0-9]+')->middleware('auth');
+    Route::get('/forum/messages/{id}', ['uses' => 'ForumController@categoryMessages', 'as' => 'forum.category.messages'])->where('id', '[0-9]+');
     Route::post('/forum/messages/post/{id}', ['uses' => 'ForumController@categoryMessagesPost', 'as' => 'forum.category.messages.post'])->where('id', '[0-9]+')->middleware('auth');
     Route::get('/forum/message/like', ['uses' => 'ForumController@messageLike', 'as' => 'forum.category.message.like'])->middleware('auth');
 
