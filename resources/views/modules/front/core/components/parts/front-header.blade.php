c6afc438b69d144ab725fafc7a007ee25009c769
Assylkhan Aldanazar <asilkhan-al@mail.ru>
2020-10-03 18:17:44 +0600

    Версия сайта для слабовидящих

--- resources/views/modules/front/core/components/parts/front-header.blade.php
+++ resources/views/modules/front/core/components/parts/front-header.blade.php
@@ -4,16 +4,16 @@
         <div class="header__inner">
             <div class="header__inner__top row justify-content-between">
                 <div class="header__social-media col-12 col-lg-3 justify-content-center">
-                    <a class="nav-link" href="https://instagram.com"> <i class="nav__social-media fa fa-instagram"></i></a>
-                    <a class="nav-link" href="https://facebook.com"> <i class="nav__social-media fa fa-facebook-f"></i></a>
-                    <a class="nav-link" href="https://vk.com"> <i class="nav__social-media fa fa-vk"></i></a>
-                    <a class="nav-link" href="https://youtube.com"> <i class="nav__social-media fa fa-youtube"></i></a>
+                    <a class="nav-link" href="https://instagram.com"> <i style="font-size: 25px" class="nav__social-media fa fa-instagram"></i></a>
+                    <a class="nav-link" href="https://facebook.com"> <i style="font-size: 25px"  class="nav__social-media fa fa-facebook-f"></i></a>
+                    <a class="nav-link" href="https://vk.com"> <i style="font-size: 25px"  class="nav__social-media fa fa-vk"></i></a>
+                    <a class="nav-link" href="https://youtube.com"> <i style="font-size: 25px"  class="nav__social-media fa fa-youtube"></i></a>
                 </div>
                 <div class="header__auth col-12 col-lg-3 justify-content-center">
                     {{--<a class="nav-link" href=""> <i class="nav__auth fa fa-search"></i></a>--}}
-                    <a class="nav-link bvi-open" href=""> <i class="nav__auth fa fa-eye-slash"></i></a>
+                    <a class="nav-link bvi-open" href=""> <i style="font-size: 25px"  class="nav__auth fa fa-eye-slash"></i></a>
                     {{--<a class="nav-link" href=""> <i class="nav__auth fa fa-language"></i></a>--}}
-                    <a class="nav-link" href="{{route('login')}}"> <i class="nav__auth fa fa-user-circle"></i></a>
+                    <a class="nav-link" href="{{route('login')}}"> <i style="font-size: 25px"  class="nav__auth fa fa-user-circle"></i></a>
                 </div>
             </div>
             <div class="header__inner__bottom navbar navbar-expand-lg ">
