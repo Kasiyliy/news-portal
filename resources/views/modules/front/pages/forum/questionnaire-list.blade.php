106c09cf25479cdffdc2a612bf97498fb79f1a7a
Assylkhan Aldanazar <asilkhan-al@mail.ru>
2020-11-01 17:28:24 +0600

    MobileLinkController changed

--- resources/views/modules/front/pages/forum/questionnaire-list.blade.php
+++ resources/views/modules/front/pages/forum/questionnaire-list.blade.php
@@ -32,7 +32,7 @@
         <div class="container justify-content-center my-4">
             <div class="row">
                 @foreach($survey as $surv)
-                    <div class="col-12 col-sm-6 col-md-3 d-flex flex-column align-items-center justify-content-center">
+                    <div class="col-12 col-sm-6 col-md-3 p-1 d-flex flex-column align-items-center justify-content-center">
                         <img class="img img-rounded" src="{{asset($surv->image_path)}}">
                         <a href="{{route('forum.questionnaire', $surv->id)}}">{{$surv->title}}</a>
                     </div>
